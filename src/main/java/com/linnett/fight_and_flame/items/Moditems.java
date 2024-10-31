3ccdbfb3ac56530fb35a3a4cb6667712c8461e96
Linnett <149827959+Linnett19@users.noreply.github.com>
2024-10-31 12:34:48 +0200

    yeah

--- src/main/java/com/linnett/fight_and_flame/items/Moditems.java
+++ src/main/java/com/linnett/fight_and_flame/items/Moditems.java
@@ -1,92 +1,105 @@
 package com.linnett.fight_and_flame.items;
 
 import com.linnett.fight_and_flame.FightAndFlame;
+import com.linnett.fight_and_flame.block.ModBlocks;
 import com.linnett.fight_and_flame.items.custom_items.Flute;
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.awt.*;
+
 public class Moditems {
     public static final DeferredRegister<Item> ITEM =
             DeferredRegister.create(ForgeRegistries.ITEMS, FightAndFlame.MOD_ID);
 
+    public static final Rarity RARITY_BLOOD_MOON = Rarity.create("fight_and_flame:blood_moon", ChatFormatting.DARK_RED);
+    public static final Rarity RARITY_NEPHRITIS = Rarity.create("fight_and_flame:nephritis", ChatFormatting.DARK_GREEN);
+    public static final Rarity RARITY_CANDY = Rarity.create("fight_and_flame:candy", style -> style.withColor(0XFDDA86));
+    public static final Rarity RARITY_RAINBOW = Rarity.create("fight_and_flame:rainbow", style -> style.withColor(Color.HSBtoRGB((System.currentTimeMillis() % 5000) / 5000F, 1f, 1F)));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT = ITEM.register("blank_blueprint" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT = ITEM.register("blank_blueprint",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_TORN = ITEM.register("blank_blueprint_torn" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_TORN = ITEM.register("blank_blueprint_torn",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_TWOHANDED_SWORD = ITEM.register("blank_blueprint_twohanded_sword" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_TWOHANDED_SWORD = ITEM.register("blank_blueprint_twohanded_sword",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_RAPIER = ITEM.register("blank_blueprint_rapier" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_RAPIER = ITEM.register("blank_blueprint_rapier",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_SICKLE = ITEM.register("blank_blueprint_sickle" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_SICKLE = ITEM.register("blank_blueprint_sickle",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_BRAID = ITEM.register("blank_blueprint_braid" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_BRAID = ITEM.register("blank_blueprint_braid",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLANK_BLUEPRINT_SPEAR = ITEM.register("blank_blueprint_spear" ,
+    public static final RegistryObject<Item> BLANK_BLUEPRINT_SPEAR = ITEM.register("blank_blueprint_spear",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> NEPHRITIS_INGOT = ITEM.register("nephritis_ingot" ,
+    public static final RegistryObject<Item> NEPHRITIS_INGOT = ITEM.register("nephritis_ingot",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> NEPHRITIS_PIECES = ITEM.register("nephritis_pieces" ,
+    public static final RegistryObject<Item> NEPHRITIS_PIECES = ITEM.register("nephritis_pieces",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> RAW_NEPHRITIS = ITEM.register("raw_nephritis" ,
+    public static final RegistryObject<Item> RAW_NEPHRITIS = ITEM.register("raw_nephritis",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> AWAKENED_NETHERITE_INGOT = ITEM.register("awakened_netherite_ingot" ,
+    public static final RegistryObject<Item> AWAKENED_NETHERITE_INGOT = ITEM.register("awakened_netherite_ingot",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> AWAKENED_NETHERITE_PIECES = ITEM.register("awakened_netherite_pieces" ,
+    public static final RegistryObject<Item> AWAKENED_NETHERITE_PIECES = ITEM.register("awakened_netherite_pieces",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLOOD_STONE_INGOT = ITEM.register("blood_stone_ingot" ,
+    public static final RegistryObject<Item> BLOOD_STONE_INGOT = ITEM.register("blood_stone_ingot",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> RAW_BLOOD_STONE = ITEM.register("raw_blood_stone" ,
+    public static final RegistryObject<Item> RAW_BLOOD_STONE = ITEM.register("raw_blood_stone",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> BLOOD_STONE_PIECES = ITEM.register("blood_stone_pieces" ,
+    public static final RegistryObject<Item> BLOOD_STONE_PIECES = ITEM.register("blood_stone_pieces",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> APHATIT_CRYSTAL = ITEM.register("aphatit_crystal" ,
+    public static final RegistryObject<Item> APHATIT_CRYSTAL = ITEM.register("aphatit_crystal",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> APHATIT_CRYSTAL_SPLINTER = ITEM.register("aphatit_crystal_splinter" ,
+    public static final RegistryObject<Item> APHATIT_CRYSTAL_SPLINTER = ITEM.register("aphatit_crystal_splinter",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> FLUTE = ITEM.register("flute" ,
+    public static final RegistryObject<Item> FLUTE = ITEM.register("flute",
             () -> new Flute(new Item.Properties()));
 
-    public static final RegistryObject<Item> NOTE = ITEM.register("note_1" ,
+    public static final RegistryObject<Item> NOTE = ITEM.register("note_1",
             () -> new Flute(new Item.Properties()));
 
-    public static final RegistryObject<Item> ELEMENTAL_CRYSTAL = ITEM.register("elemental_crystal" ,
+    public static final RegistryObject<Item> ELEMENTAL_CRYSTAL = ITEM.register("elemental_crystal",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> CAULDRON = ITEM.register("cauldron",
+            () -> new ItemNameBlockItem(ModBlocks.CAULDRON.get(), new Item.Properties().rarity(RARITY_RAINBOW)));
+
+    public static final RegistryObject<Item> CANDICORN = ITEM.register("candicorn",
+            () -> new Item(new Item.Properties().rarity(RARITY_CANDY)));
+
+    public static final RegistryObject<Item> SLIME_LOLIPOP = ITEM.register("slime_lolipop",
+            () -> new Item(new Item.Properties().rarity(RARITY_CANDY)));
+
+    public static final RegistryObject<Item> SOUR_BONE = ITEM.register("sour_bone",
+            () -> new Item(new Item.Properties().rarity(RARITY_CANDY)));
+
+    public static final RegistryObject<Item> JELLY_BALLS = ITEM.register("jelly_balls",
+            () -> new Item(new Item.Properties().rarity(RARITY_CANDY)));
 
 
-    public static final RegistryObject<Item> CANDICORN = ITEM.register("candicorn" ,
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> SLIME_LOLIPOP = ITEM.register("slime_lolipop" ,
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> SOUR_BONE  = ITEM.register("sour_bone" ,
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> JELLY_BALLS = ITEM.register("jelly_balls" ,
-            () -> new Item(new Item.Properties()));
-
-
-
-    public static void register (IEventBus eventBus) {
+    public static void register(IEventBus eventBus) {
         ITEM.register(eventBus);
     }
 }
--- src/main/java/com/linnett/fight_and_flame/particles/custom/SplashParticleTemplate.java
+++ src/main/java/com/linnett/fight_and_flame/particles/custom/SplashParticleTemplate.java
@@ -8,21 +8,24 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class SplashParticleTemplate extends TextureSheetParticle {
     private final SpriteSet sprites;
+    private final double initialYSpeed;
+
     protected SplashParticleTemplate(ClientLevel pLevel, double pX, double pY, double pZ, SpriteSet spriteSet, double pXSpeed, double pYSpeed, double pZSpeed) {
         super(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed);
-        this.gravity = 0.01F;
-        this.friction = 0.95F;
-        this.xd = pXSpeed;
-        this.yd = pYSpeed;
-        this.zd = pZSpeed;
+        this.gravity = 1F;
+        this.friction = 1F;
+        this.xd = 0;
+        this.zd = 0;
+        this.initialYSpeed = Math.abs(pYSpeed) * 0.5;
+        this.yd = initialYSpeed;
         this.sprites = spriteSet;
         this.quadSize *= 2f;
-        this.lifetime = 40;
+        this.lifetime = 20;
 
-        this.rCol =1f;
-        this.gCol=1f;
-        this.bCol=1f;
-        this.roll = (float) Math.random() * ((float) Math.PI)/4-((float)Math.PI*0.125f);
+        this.rCol = 1f;
+        this.gCol = 1f;
+        this.bCol = 1f;
+        this.roll = (float) Math.random() * ((float) Math.PI) / 4 - ((float) Math.PI * 0.125f);
         this.oRoll = this.roll;
         this.setSpriteFromAge(spriteSet);
         this.alpha = 0.6F;
@@ -32,22 +35,34 @@ public class SplashParticleTemplate extends TextureSheetParticle {
     public void tick() {
         super.tick();
         this.setSpriteFromAge(this.sprites);
-        this.alpha = 0.6f-((float) this.age/(float)this.lifetime)*0.6f;
-    }
 
+
+        this.alpha = 0.6f - ((float) this.age / (float) this.lifetime) * 0.6f;
+
+
+        if (this.age < this.lifetime / 2) {
+            this.yd = initialYSpeed * (1 - (float) this.age / (this.lifetime / 2)); // Снижение скорости подъема
+        } else {
+
+            this.yd -= 0.05;
+        }
+    }
 
     @OnlyIn(Dist.CLIENT)
     public static class Provider implements ParticleProvider<SimpleParticleType> {
         private final SpriteSet sprites;
+
         public Provider(SpriteSet spriteSet) {
             this.sprites = spriteSet;
         }
+
         public Particle createParticle(SimpleParticleType particleType, ClientLevel level,
                                        double x, double y, double z,
                                        double dx, double dy, double dz) {
             return new SplashParticleTemplate(level, x, y, z, this.sprites, dx, dy, dz);
         }
     }
+
     @Override
     public ParticleRenderType getRenderType() {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
